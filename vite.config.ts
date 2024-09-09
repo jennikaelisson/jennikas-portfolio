05451710e311ab1aa6f354d0a6bebbc5479d254e
Jennika Elisson <jennika.elisson@gmail.com>
2024-09-09 11:30:26 +0200

    removde gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/"
-})
+  base: '/jennikas-portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notFound: resolve(__dirname, '404.html')
+      }
+    }
+  }
+});
